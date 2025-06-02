0c578246d377552c6a9b6138a2301fd7e6203306
Venu <venu@nexthop.ai>
2025-06-02 18:46:40 +0000

    Fix fib test issues: handle ecmp inner hash support and other fixes

--- ansible/roles/test/files/ptftests/py3/hash_test.py
+++ ansible/roles/test/files/ptftests/py3/hash_test.py
@@ -247,11 +247,11 @@ class HashTest(BaseTest):
 
     def _get_ip_proto(self, ipv6=False):
         # ip_proto 2 is IGMP, should not be forwarded by router
-        # ip_proto 4 and 41 are encapsulation protocol, ip payload will be malformat
+        # ip_proto 4, 41 and 47 are encapsulation protocol, ip payload will be malformat
         # ip_proto 60 is redirected to ip_proto 4 as encapsulation protocol, ip payload will be malformat
         # ip_proto 254 is experimental
         # MLNX ASIC can't forward ip_proto 254, BRCM is OK, skip for all for simplicity
-        skip_protos = [2, 253, 4, 41, 60, 254]
+        skip_protos = [2, 253, 4, 41, 47, 60, 254]
 
         if self.is_active_active_dualtor:
             # Skip ICMP for active-active dualtor as it is duplicated to both ToRs
@@ -631,6 +631,11 @@ class IPinIPHashTest(HashTest):
     for IPinIP packet.
     '''
 
+    def setUp(self):
+        HashTest.setUp(self)
+        self.ecmp_inner_header_hash_supported = self.test_params.get(
+            'ecmp_inner_header_hash_supported', False)
+
     def check_ipv4_route(self, hash_key, src_port, dst_port_lists, outer_src_ip, outer_dst_ip):
         '''
         @summary: Check IPv4 route works.
@@ -900,8 +905,9 @@ class IPinIPHashTest(HashTest):
             logging.info("hash_key={}, hit count map: {}".format(
                 hash_key, hit_count_map))
 
-            for next_hop in next_hops:
-                self.check_balancing(next_hop.get_next_hop(), hit_count_map, src_port, hash_key)
+            if self.ecmp_inner_header_hash_supported:
+                for next_hop in next_hops:
+                    self.check_balancing(next_hop.get_next_hop(), hit_count_map, src_port, hash_key)
 
     def runTest(self):
         """
@@ -1208,6 +1214,11 @@ class NvgreHashTest(HashTest):
     for NvGRE packet.
     '''
 
+    def setUp(self):
+        HashTest.setUp(self)
+        self.ecmp_inner_header_hash_supported = self.test_params.get(
+            'ecmp_inner_header_hash_supported', False)
+
     def simple_nvgrev6_packet(self, pktlen=300,
                               eth_dst='00:01:02:03:04:05',
                               eth_src='00:06:07:08:09:0a',
@@ -1504,8 +1515,9 @@ class NvgreHashTest(HashTest):
         logging.info("hash_key={}, hit count map: {}".format(
             hash_key, hit_count_map))
 
-        for next_hop in next_hops:
-            self.check_balancing(next_hop.get_next_hop(), hit_count_map, src_port, hash_key)
+        if self.ecmp_inner_header_hash_supported:
+            for next_hop in next_hops:
+                self.check_balancing(next_hop.get_next_hop(), hit_count_map, src_port, hash_key)
 
     def runTest(self):
         """
